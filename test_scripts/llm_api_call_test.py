52f64ce8fd69d029d70b6bb1861f449772bf9dc4
allen327lin <allen327lin2@gmail.com>
2025-01-02 12:45:50 +0800

    feat: moved test scripts and revised them

--- test_scripts/llm_api_call_test.py
+++ test_scripts/llm_api_call_test.py
@@ -1,10 +1,30 @@
 from dotenv import load_dotenv
 import os
 from langchain_groq import ChatGroq
+from pathlib import Path
+
+# 刪除環境變量
+if "GROQ_API_KEY" in os.environ:
+    del os.environ["GROQ_API_KEY"]
+
+# 找根目錄
+def find_project_root(current_path, marker=".git"):
+    current_path = Path(current_path).resolve()
+    for parent in current_path.parents:
+        if (parent / marker).exists():
+            return parent
+    return None
+
+current_path = __file__
+project_root = find_project_root(current_path, marker=".git")
+print("Project root:", project_root)
 
 # Load .env file
-load_dotenv()
+print(f"Successfully loaded env variables: {load_dotenv(project_root / ".env")}")
 
+# Load env variables into python variables
+print("Loaded env variables:")
+print(f"GROQ_API_KEY = {os.getenv("GROQ_API_KEY")}")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 llm = ChatGroq(
